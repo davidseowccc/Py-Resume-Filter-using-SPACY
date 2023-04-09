d3cf9c75390f13fa5c046ea0576488f733750f31
davidseowccc <62555082+davidseowccc@users.noreply.github.com>
2023-04-09 17:23:31 +0800

    Add files via upload

---
+++ PhraseCatcher_EAEr4clean.py
@@ -0,0 +1,139 @@
+#EAE Keywords Matcher code
+#For AEG example with 265 applicants,
+#it takes about 5 min to run the prog in Python3 shell.
+
+
+#importing all required libraries
+
+import PyPDF2
+import os
+from os import listdir
+from os.path import isfile, join
+from io import StringIO
+import pandas as pd
+from collections import Counter
+import en_core_web_sm
+nlp = en_core_web_sm.load()
+from spacy.matcher import PhraseMatcher
+
+#Function to read resumes from the folder one by one
+mypath="/home/david/PYenv/Resume_scr/NLP_Resume/EAEapp" #enter your path here where you saved the resumes
+onlyfiles = [os.path.join(mypath, f) for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+
+def pdfextract(file):
+    fileReader = PyPDF2.PdfReader(open(file,'rb'))
+    countpage = len(fileReader.pages)
+    count = 0
+    text = []
+    while count < countpage:    
+        pageObj = fileReader.pages[count]
+        count +=1
+        t = pageObj.extract_text()
+        print (t)
+        text.append(t)
+    return text
+
+#function to read resume ends
+
+
+#function that does keyword(s) matching and builds a candidate profile
+def create_profile(file):
+    text = pdfextract(file) 
+    text = str(text)
+    #text = text.replace("\\n", "")
+    text = text.lower()
+    #below is the csv where we have all the keywords, you can customize your own
+    keyword_dict = pd.read_csv('/home/david/PYenv/Resume_scr/NLP_Resume/template/template.csv', encoding='ISO-8859-1')
+    math_words = [nlp(text) for text in keyword_dict['Math'].dropna(axis = 0)]
+    eng_words = [nlp(text) for text in keyword_dict['Engineering'].dropna(axis = 0)]
+    prog_words = [nlp(text) for text in keyword_dict['Program'].dropna(axis = 0)]
+    AEG_words = [nlp(text) for text in keyword_dict['AEG'].dropna(axis = 0)]
+    X_words = [nlp(text) for text in keyword_dict['X factor'].dropna(axis = 0)]
+    Poor_words = [nlp(text) for text in keyword_dict['Poor Perf'].dropna(axis = 0)]
+    Avg_words = [nlp(text) for text in keyword_dict['Avg Perf'].dropna(axis = 0)]
+    AAvg_words = [nlp(text) for text in keyword_dict['Above Avg Perf'].dropna(axis = 0)]
+    VG_words = [nlp(text) for text in keyword_dict['V.G Perf'].dropna(axis = 0)]
+    
+    matcher = PhraseMatcher(nlp.vocab)
+    matcher.add('math', None, *math_words)
+    matcher.add('eng', None, *eng_words)
+    matcher.add('prog', None, *prog_words)
+    matcher.add('AEG', None, *AEG_words)
+    matcher.add('X', None, *X_words)
+    matcher.add('Poor', None, *Poor_words)
+    matcher.add('Avg', None, *Avg_words)
+    matcher.add('Above_Avg', None, *AAvg_words)
+    matcher.add('Very_Good', None, *VG_words)
+    doc = nlp(text)
+    
+    d = []  
+    matches = matcher(doc)
+    for match_id, start, end in matches:
+        rule_id = nlp.vocab.strings[match_id]  # get the unicode ID, i.e. 'COLOR'
+        span = doc[start : end]  # get the matched slice of the doc
+        d.append((rule_id, span.text))      
+    keywords = "\n".join(f'{i[0]} {i[1]} ({j})' for i,j in Counter(d).items())
+    
+    ## converting string of keywords to dataframe
+    df = pd.read_csv(StringIO(keywords),names = ['Keywords_List'])
+    df1 = pd.DataFrame(df.Keywords_List.str.split(' ',n=1).tolist(),columns = ['Subject','Keyword'])
+    df2 = pd.DataFrame(df1.Keyword.str.split('(',n=1).tolist(),columns = ['Keyword', 'Count'])
+    df3 = pd.concat([df1['Subject'],df2['Keyword'], df2['Count']], axis =1) 
+    df3['Count'] = df3['Count'].apply(lambda x: x.rstrip(")"))
+    
+    base = os.path.basename(file)
+    filename = os.path.splitext(base)[0]
+       
+    name = filename.split('_')
+    name2 = name[0]
+    name2 = name2.lower()
+
+    ## converting str to dataframe
+    name3 = pd.read_csv(StringIO(name2),names = ['Candidate Name'])
+    
+    dataf = pd.concat([name3['Candidate Name'], df3['Subject'], df3['Keyword'], df3['Count']], axis = 1)
+    dataf['Candidate Name'].fillna(dataf['Candidate Name'].iloc[0], inplace = True)
+
+    return(dataf)
+        
+#function ends
+        
+#code to execute/call the above functions
+
+final_database=pd.DataFrame()
+i = 0 
+while i < len(onlyfiles):
+    file = onlyfiles[i]
+    dat = create_profile(file)
+    final_database = pd.concat([final_database, dat], ignore_index=True)
+    i +=1
+    print(final_database)
+
+    
+#code to count words under each category and visulaize it through Matplotlib
+final_database2 = final_database['Keyword'].groupby([final_database['Candidate Name'], final_database['Subject']]).count().unstack()
+final_database2.reset_index(inplace = True)
+final_database2.fillna(0,inplace=True)
+new_data = final_database2.iloc[:,1:]
+new_data.index = final_database2['Candidate Name']
+
+#execute the below line to see the candidate profile in a csv format
+sample2=new_data.to_csv('/home/david/PYenv/Resume_scr/NLP_Resume/template/EAEselect.csv')
+
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 10})
+ax = new_data.plot.barh(title="EAE_AEG keywords by category", legend=False, figsize=(25,7), stacked=True)
+labels = []
+for j in new_data.columns:
+    for i in new_data.index:
+        label = str(j)+": " + str(new_data.loc[i][j])
+        labels.append(label)
+patches = ax.patches
+for label, rect in zip(labels, patches):
+    width = rect.get_width()
+    if width > 0:
+        x = rect.get_x()
+        y = rect.get_y()
+        height = rect.get_height()
+        ax.text(x + width/2., y + height/2., label, ha='center', va='center')
+plt.show()
